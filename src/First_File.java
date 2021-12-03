682b6fad73b053a6563a9e1cd094c99881c0256d
Noor Ahmad <noorahmedspider@gmail.com>
2021-12-03 19:41:37 +0500

    second commit

--- src/First_File.java
+++ src/First_File.java
@@ -4,6 +4,10 @@ public class First_File {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String name = "Noor Ahmad";
+		String rollno = "20i-2343";
+		System.out.println("Name: " + name);
+		System.out.println("RollNo: " + rollno);
 	}
 
 }
