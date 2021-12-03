6ddf5d183418adcec2895945e53b9fea4d3b9ee6
Usman Sharjeel <i200880@nu.edu.pk>
2021-12-03 20:25:47 +0500

    Update First_File.java

--- src/First_File.java
+++ src/First_File.java
@@ -1,14 +1,12 @@
-
 public class First_File {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		String name = "Usman sharjeel";
 		String rollno = "20I-0880";
 		System.out.println("Name: " + name);
 		System.out.println("RollNo: " + rollno);
-		System.out.println("Branch : Usman Branch ");
+		
 	}
 
 }
